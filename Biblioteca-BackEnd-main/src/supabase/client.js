afb3350ddf3bc01d32177d311eeb33746231869c
Juan Diego Tangarife Montoya <149004102+JuanTangarife0814@users.noreply.github.com>
2026-04-29 13:23:26 -0500

    Delete Biblioteca-BackEnd-main directory

--- Biblioteca-BackEnd-main/src/supabase/client.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-import { createClient } from '@supabase/supabase-js'
-
-export const client = createClient(
-    "https://rvjswwtifhdxgzlxipbo.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2anN3d3RpZmhkeGd6bHhpcGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwMzE0NjksImV4cCI6MjA3NDYwNzQ2OX0.MrIhE5DJu4q6u4JYSrZObFOzETz53ir2YHHqV6kFbQg"
-
-);
